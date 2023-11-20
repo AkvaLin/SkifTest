23bb75256744e804ded9191be07db531f4d09bac
Nikita Pivovarov <nikita.pivovarov2003@gmail.com>
2023-11-20 12:27:55 +0300

    initial setup

---
+++ SkifTest/SkifTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SkifTest
+//
+//  Created by Никита Пивоваров on 20.11.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
